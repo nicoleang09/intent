746627e5bae25372d4f89a714e5a4f36335b0b52
Nicole <60417365+nicoleang09@users.noreply.github.com>
2025-08-05 22:48:11 +0800

    Migrate to Vite

--- frontend/src/utils/Utils.tsx
+++ frontend/src/utils/Utils.tsx
@@ -1,22 +1,45 @@
-export const dayList = ["Monday", "Tuesday", "Wednesday",
-  "Thursday", "Friday", "Saturday", "Sunday"];
+export const dayList = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+];
 
-export const hourList = ["0700", "0800", "0900",
-  "1000", "1100", "1200", "1300", "1400", "1500",
-  "1600", "1700", "1800", "1900", "2000", "2100",
-  "2200", "2300"];
+export const hourList = [
+  '0700',
+  '0800',
+  '0900',
+  '1000',
+  '1100',
+  '1200',
+  '1300',
+  '1400',
+  '1500',
+  '1600',
+  '1700',
+  '1800',
+  '1900',
+  '2000',
+  '2100',
+  '2200',
+  '2300',
+];
 
-export const apiUrl = process.env.REACT_APP_API_URL;
+export const apiUrl = import.meta.env.VITE_API_URL;
 
 export const getCookie = async () => {
   let cookie;
-  
-  await fetch(apiUrl + "/getcookie", {
-    method: "GET",
-    mode: "cors",
-    credentials: "include",
-  }).then((res) => res.json())
-  .then((data) => cookie = data);
-  
+
+  await fetch(apiUrl + '/getcookie', {
+    method: 'GET',
+    mode: 'cors',
+    credentials: 'include',
+  })
+    .then((res) => res.json())
+    .then((data) => (cookie = data));
+
   return cookie;
 };
